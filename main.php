90866e62af371badd48c4e6aef8523775a971b4e
Yuji Tokiwa <yuji.tokiwa.dc@hosei.ac.jp>
2021-02-07 11:13:36 +0900

    almost function implemented

--- main.php
+++ main.php
@@ -1,59 +1,18 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>TGLE</title>
-    <!--    Bootstrap begin-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
-    <!--    Bootstrap end-->
-</head>
-
-<body>
-<div class="container">
-    <div class="jumbotron">
-        <h1 class="text-center">TGLE</h1>
-        <p class="text-center">Tools for Group Learning Environment</p>
-    </div>
-
     <?php
     require_once __DIR__ . '/vendor/autoload.php';
     require_once __DIR__ . '/db/example_database.php';
 
     use \IMSGlobal\LTI;
 
-// Retrieve Data from jwt
     $launch = LTI\LTI_Message_Launch::new(new Example_Database())
         ->validate();
+    //
+    // $launch gets jwt which includes id_token and jwt
+    // In detail, you can inspect data by IDE such as PhpStorm
+    //
 
-    $user_id = $launch->get_launch_data()['https://purl.imsglobal.org/spec/lti/claim/ext']['user_username'];
-    $course_id = $launch->get_launch_data()['https://purl.imsglobal.org/spec/lti/claim/context']['label'];
+    $redirect_url = "/tgle_main.php?launch_id=".$launch->get_launch_id();
 
-    echo "<h3 class='text-center'>" . $launch->get_launch_data()['https://purl.imsglobal.org/spec/lti/claim/context']['title'] . "</h3>";
-    echo "<p class='text-center'>Course ID: " . $course_id . "</p>";
-    echo "<p class='text-center'>User ID: " . $user_id . "</p>";
-    echo "<p class='text-center'>Name: " . $launch->get_launch_data()['name'] . "</p>";
-    echo "<p class='text-center'>Mail: " . $launch->get_launch_data()['email'] . "</p>";
-
+    header("Location:".$redirect_url);
+    exit();
     ?>
-
-    <p style="line-height : 20px;">　</p>
-
-    <h3 class='text-center'><a href="tgle_group.php?user_id=<?= $user_id ?>&course_id=<?= $course_id ?>"> グループ/座席確認 </a>
-    </h3>
-    <h3 class='text-center'> Keyword入力</h3>
-
-</div>
-</body>
-</html>
